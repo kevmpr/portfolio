c6b8d4c8c269c7a70d1a193f790e9aab794f6171
Kevin Maximiliano Palma Romero <kev.mpr03@gmail.com>
2026-02-25 23:41:34 -0300

    fix: remove base path config breaking Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://kevmpr.github.io',
-  base: '/portfolio/',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'es',
